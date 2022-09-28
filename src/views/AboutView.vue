d82e070bfd5ef4a8215bd96aa46079be8788d163
Sabelo-Mdashe <selbysabelomdashe@gmail.com>
2022-09-28 09:56:07 +0200

    first commit

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, animi odit illo voluptates voluptatem debitis vel autem consectetur, itaque molestias at suscipit quod cum obcaecati corporis non sapiente, explicabo eum? Officia doloremque magnam nemo molestiae blanditiis id temporibus ipsa recusandae.</p>
   </div>
 </template>
---
+++ src/views/Todo.vue
@@ -0,0 +1,96 @@
+<template>
+  <div>
+    <v-text-field 
+    @click:append="addTodo"
+    @keyup.enter="addTodo"
+    v-model="newTodo"
+    class="pa-3"
+              outlined
+              label="Add Todo"
+              append-icon="mdi-plus"
+              hide-details
+              clearable
+            ></v-text-field>
+    <v-list
+      flat
+      class="pt-0"
+    >
+<div v-for="todo in todos" :key="todo.id">
+
+
+  <v-list-item class="pt-0" @click="doneTodo(todo.id)" :class="{'blue lighten-4': todo.done}">
+    <template v-slot:default>
+      <v-list-item-action>
+        <v-checkbox
+          :input-value="todo.done"
+          color="primary"
+        ></v-checkbox>
+      </v-list-item-action>
+
+      <v-list-item-content>
+        <v-list-item-title :class="{'text-decoration-line-through': todo.done}">{{todo.title}}</v-list-item-title>
+      </v-list-item-content>
+      <v-list-item-action>
+          <v-btn icon @click.stop="deleteTodo(todo.id)">
+            <v-icon color="primary">mdi-delete</v-icon>
+          </v-btn>
+        </v-list-item-action>
+    </template>
+  </v-list-item>
+  <v-divider></v-divider>
+</div>
+    </v-list>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        newTodo: '',
+        todos: [
+          {
+            id: 1,
+            title: 'Code',
+            done: false
+          },
+          {
+            id: 2,
+            title: 'Eat',
+            done: false
+          },
+          {
+            id: 3,
+            title: 'Sleep',
+            done: false
+          },
+          {
+            id: 4,
+            title: 'Repeat',
+            done: false
+          }
+        ]
+      }
+    },
+    methods: {
+      doneTodo(id) {
+        let todo = this.todos.filter(todo => todo.id === id)[0]
+        todo.done = !todo.done
+      },
+
+      deleteTodo(id) {
+        this.todos = this.todos.filter(todo => todo.id !== id)
+      },
+
+      addTodo() {
+        let newTodo = {
+          id: this.todos.length + 1,
+          title: this.newTodo,
+          done: false
+        }
+
+        this.todos.push(newTodo)
+      }
+    }
+  }
+</script>
